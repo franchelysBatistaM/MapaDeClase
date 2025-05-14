5614ec4f235d390168e7fbd52a662c37464c28a3
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-14 16:15:08 -0400

    Creando Clase Estudiante

---
+++ mapaDeClase/MapaDeClase/Entities/Estudiante.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaDeClase.Entities
+{
+    public class Estudiante : MiembroDeLaComunidad
+    {
+        public string Carrera { get; set; }
+        public int Matricula { get; set; }
+        public Estudiante(string nombre, string apellido, int edad, string carrera, int matricula)
+            : base(nombre, apellido, edad)
+        {
+            Carrera = carrera;
+            Matricula = matricula;
+        }
+        public override void presentar()
+        {
+            base.presentar();
+            Console.WriteLine($"este es su carrera: {Carrera}");
+            Console.WriteLine($"este es su matricula: {Matricula}");
+        }
+    }
+}
