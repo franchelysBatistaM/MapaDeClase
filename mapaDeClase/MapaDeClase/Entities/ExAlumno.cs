c11863802a05d30aab0cbf24850fbd1e7eb522df
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-14 16:18:59 -0400

    Creando Clase ExAlumno

---
+++ mapaDeClase/MapaDeClase/Entities/ExAlumno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaDeClase.Entities
+{
+    public class Exalumno : MiembroDeLaComunidad
+    {
+        public string Fechainicio { get; set; }
+        public string Fechatermino { get; set; }
+        public Exalumno(string nombre, string apellido, int edad, string fechainicio, string fechatermino)
+            : base(nombre, apellido, edad)
+        {
+            Fechainicio = fechainicio;
+            Fechatermino = fechatermino;
+        }
+        public override void presentar()
+        {
+            base.presentar();
+            Console.WriteLine($"empezo en: {Fechainicio}");
+            Console.WriteLine($"termino en: {Fechatermino}");
+        }
+    }
+}
