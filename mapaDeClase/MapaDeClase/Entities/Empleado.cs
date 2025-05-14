d4947f6662f81a13b3cf58ecef0f8e84b174d5c6
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-14 16:14:35 -0400

    Creando Clase Empleado

---
+++ mapaDeClase/MapaDeClase/Entities/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaDeClase.Entities
+{
+    public class Empleado : MiembroDeLaComunidad
+    {
+        public decimal Salario { get; set; }
+        public string Puesto { get; set; }
+        public Empleado(string nombre, string apellido, int edad, decimal salario, string puesto)
+            : base(nombre, apellido, edad)
+        {
+            Salario = salario;
+            Puesto = puesto;
+        }
+        public override void presentar()
+        {
+            base.presentar();
+            Console.WriteLine($"Este es su salario: {Salario}");
+            Console.WriteLine($"Este es su puesto: {Puesto}");
+        }
+    }
+}
