d2009ddaea60a076726a2eb77e3ae5df6b126d0b
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-14 16:12:41 -0400

    Creando Clase MiembroDeLaComunidad

---
+++ mapaDeClase/MapaDeClase/Entities/MiembroDeLaComunidad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaDeClase.Entities
+{
+    public class MiembroDeLaComunidad
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public MiembroDeLaComunidad(string nombre, string apellido, int edad)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+        }
+        public virtual void presentar()
+        {
+            Console.WriteLine($"El Nombre es: {Nombre} ");
+            Console.WriteLine($"El Apellido es: {Apellido} ");
+            Console.WriteLine($"La Edad es: {Edad} ");
+        }
+    }
+}
