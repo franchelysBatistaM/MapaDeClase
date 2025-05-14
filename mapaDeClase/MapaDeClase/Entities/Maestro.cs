c8577b7e4ab056bc251a911af13c03f4dee0de8c
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-14 16:20:53 -0400

    Creando Clase Maestro

---
+++ mapaDeClase/MapaDeClase/Entities/Maestro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaDeClase.Entities
+{
+    public class Maestro : Docente
+    {
+        public int Yeardeeduca { get; set; }
+        public Maestro(string nombre, string apellido, int edad, decimal salario, string puesto, string especialidad, int telefono, int yeardeeduca)
+            : base(nombre, apellido, edad, salario, puesto, especialidad, telefono)
+        {
+            Yeardeeduca = yeardeeduca;
+        }
+        public override void presentar()
+        {
+            base.presentar();
+            Console.WriteLine($"esto es el años ejercido: {Yeardeeduca}");
+        }
+
+    }
+}
