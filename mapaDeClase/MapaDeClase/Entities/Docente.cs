fa9a6b5bc638102573f1c6ccbbab75c2538be172
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-14 16:19:24 -0400

    Creando Clase Docente

---
+++ mapaDeClase/MapaDeClase/Entities/Docente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaDeClase.Entities
+{
+    public class Docente : Empleado
+    {
+        public string Especialidad { get; set; }
+        public int Telefono { get; set; }
+        public Docente(string nombre, string apellido, int edad, decimal salario, string puesto, string especialidad, int telefono)
+            : base(nombre, apellido, edad, salario, puesto)
+        {
+            Especialidad = especialidad;
+            Telefono = telefono;
+        }
+        public override void presentar()
+        {
+            base.presentar();
+            Console.WriteLine($"su especialidad es: {Especialidad}");
+            Console.WriteLine($"su telefono es: {Telefono}");
+        }
+    }
+}
