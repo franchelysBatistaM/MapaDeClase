5ea78c579981bbdff026fa888d2486385c1d58c8
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-14 16:19:54 -0400

    Creando Clase Administrativo

---
+++ mapaDeClase/MapaDeClase/Entities/Administrativo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaDeClase.Entities
+{
+    public class Administrativo : Docente
+    {
+        public string Posicion { get; set; }
+        public Administrativo(string nombre, string apellido, int edad, decimal salario, string puesto, string especialidad, int telefono, string posicion)
+            : base(nombre, apellido, edad, salario, puesto, especialidad, telefono)
+        {
+            Posicion = posicion;
+
+        }
+        public override void presentar()
+        {
+            base.presentar();
+            Console.WriteLine($"este es su posicion: {Posicion}");
+        }
+    }
+
+}
