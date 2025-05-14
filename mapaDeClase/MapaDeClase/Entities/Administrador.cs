0ec9db9038b18849ee8e30627e82f42041867c85
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-14 16:20:26 -0400

    Creando Clase Administrador

---
+++ mapaDeClase/MapaDeClase/Entities/Administrador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaDeClase.Entities
+{
+    public class Administrador : Docente
+    {
+        public string Cargo { get; set; }
+        public Administrador(string nombre, string apellido, int edad, decimal salario, string puesto, string especialidad, int telefono, string cargo)
+            : base(nombre, apellido, edad, salario, puesto, especialidad, telefono)
+        {
+            Cargo = cargo;
+        }
+        public override void presentar()
+        {
+            base.presentar();
+            Console.WriteLine($"este es su cargo: {Cargo}");
+        }
+    }
+
+}
